1ebebeae516d53a70291ade2b9d5ffb4481379fa
Enyer045 <enyerber045gmail.com>
2025-04-09 19:00:03 -0400

    Primer vistazo a la estructura de Vue

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,49 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+  const titulo =  'Prueba de titulo 1' // Esto es solo una constante
+
+  // Haciendo preba de una funcion sencilla 
+  const buscarCosa = () =>{
+    console.log('Estamos buscando la cosa');
+  }
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="flex items-center flex-col p-5">
+    <h1 class="text-xl m-4">{{ titulo }}</h1> <!-- Aca llamamos a la constante del sector script -->
+    <div class="flex items-center">
+      <!--Esto es una input traido desde tailwind-->
+      <input 
+      type="text" 
+      class="block min-w-0 grow py-1.5 pr-3 pl-1 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6" placeholder="Buscar...">
+      <!-- Para colocar un evento Onclick solo hay que colocar @click -->
+      <button @click = 'buscarCosa()' class="bg-blue-500 py-1.5 px-5" >Buscar</button> <!-- Aca se está haciendo uso de la función-->
+    </div>
+    <div class="m-6">
+      <table class="table-fixed">
+        <thead>
+          <th>Foto</th>
+          <th>Nombre</th>
+          <th>Número</th>
+          <th>Correo</th>
+          <th>Acción</th>
+        </thead>
+        <tbody>
+          <tr v-for="n in 20"> <!--Empezamos a usar las herramientas de Vue-->
+            <td class="p-3 border-b-2">{{ n }}</td> <!--Se usa con el for-->
+            <td class="p-3 border-b-2">Pepito</td>
+            <td class="p-3 border-b-2">04123212121</td>
+            <td class="p-3 border-b-2">correo@gmail.com</td>
+            <td>
+              <a href="#" class="py-2 px-4 bg-purple-900 rounded-full">Editar</a>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
+
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+
 </style>
